7340544a60d03237168a939b0930202b2a5bd750
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-04-04 22:37:37 +0530

    [kafka-Spring-Boot][S2]-#10 - Create consumer for json and specific topic for json object

--- springboot-Kafka-1/src/main/java/com/matuga/springbootKafka1/util/KafkaConstants.java
+++ springboot-Kafka-1/src/main/java/com/matuga/springbootKafka1/util/KafkaConstants.java
@@ -1,6 +1,7 @@
 package com.matuga.springbootKafka1.util;
 
 public class KafkaConstants {
+
   public static final String TOPIC_MATUGA = "matuga";
   public static final String TOPIC_MATUGA_JSON = "matugaJson";
 }
