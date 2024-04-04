3dcec5fc29938b7b49e73332c2b6838294f3d9e7
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-04-04 21:33:09 +0530

    [kafka-Spring-Boot][S2]-#9 - Create rest endpoint to send json object

--- springboot-Kafka-1/src/main/java/com/matuga/springbootKafka1/controller/MessageController.java
+++ springboot-Kafka-1/src/main/java/com/matuga/springbootKafka1/controller/MessageController.java
@@ -2,13 +2,11 @@ package com.matuga.springbootKafka1.controller;
 
 import static com.matuga.springbootKafka1.util.KafkaConstants.TOPIC_MATUGA;
 
+import com.matuga.springbootKafka1.model.User;
 import com.matuga.springbootKafka1.service.KafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/kafka")
@@ -16,14 +14,17 @@ public class MessageController {
 
   @Autowired private KafkaProducer kafkaProducer;
 
-  public MessageController(KafkaProducer kafkaProducer) {
-    this.kafkaProducer = kafkaProducer;
-  }
-
   // http://localhost:8080/api/v1/kafka/publish?message=learning_Kafka
   @GetMapping("/publish")
   public ResponseEntity<String> publishMessage(@RequestParam("message") String message) {
     kafkaProducer.sendMessage(message);
     return ResponseEntity.ok("Message " + message + " sent to topic " + TOPIC_MATUGA);
+  }
+
+  // http://localhost:8080/api/v1/kafka/publish
+  @PostMapping("/publish-user")
+  public ResponseEntity<String> publishUser(@RequestBody User user) {
+    kafkaProducer.sendMessage(user);
+    return ResponseEntity.ok("Message " + user.toString() + " sent to topic " + TOPIC_MATUGA);
   }
 }
--- springboot-Kafka-1/src/main/java/com/matuga/springbootKafka1/service/KafkaProducer.java
+++ springboot-Kafka-1/src/main/java/com/matuga/springbootKafka1/service/KafkaProducer.java
@@ -2,24 +2,38 @@ package com.matuga.springbootKafka1.service;
 
 import static com.matuga.springbootKafka1.util.KafkaConstants.TOPIC_MATUGA;
 
+import com.matuga.springbootKafka1.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
   private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
-  private KafkaTemplate<String, String> kafkaTemplate;
+  @Autowired private KafkaTemplate<String, String> kafkaTemplate;
+  @Autowired private KafkaTemplate<String, User> kafkaUserTemplate;
 
-  public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
-    this.kafkaTemplate = kafkaTemplate;
-  }
+  //  public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+  //    this.kafkaTemplate = kafkaTemplate;
+  //  }
 
   public void sendMessage(String message) {
     LOGGER.info(String.format("Message sent %s ", message));
     for (int i = 1; i < 11; i++) {
       kafkaTemplate.send(TOPIC_MATUGA, message + " :" + i);
     }
+  }
+
+  public void sendMessage(User data) {
+
+    LOGGER.info(String.format("Message sent %s", data.toString()));
+    Message<User> message =
+        MessageBuilder.withPayload(data).setHeader(KafkaHeaders.TOPIC, TOPIC_MATUGA).build();
+    kafkaUserTemplate.send(message);
   }
 }
